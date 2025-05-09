1b15e9e012137d0fc72abf2f42b1e927715d9816
Reuben Green <reubengreen183@gmail.com>
2025-05-09 11:59:22 +0100

    Remove dummy test from tests/Session.tests.cpp

--- tests/Session.tests.cpp
+++ tests/Session.tests.cpp
@@ -1,9 +1,2 @@
 #include "testsys.h"
 #include "../Session.h"
-
-TESTFUNC(msg)
-{
-  Session session("testing");
-  TESTASSERT(session.msg() == std::string("testing")); // this test should pass
-  TESTASSERT(session.msg() == std::string("wrong string")); // this test should fail
-}

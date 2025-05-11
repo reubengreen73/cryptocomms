d27d92b4cb88276114c9c148f8f7344a1edb84fc
Reuben Green <reubengreen183@gmail.com>
2025-05-11 21:36:56 +0100

    Added TESTTHROW macro to test system to allow checking that expected exceptions are thrown.

--- tests/testsys.h
+++ tests/testsys.h
@@ -4,6 +4,7 @@
 
 #include <stdexcept>
 #include <string>
+#include <stdexcept>
 
 /* The TESTFUNC macro does more than just generate a function
  * signature with a slightly mangled name. It tells the python
@@ -23,3 +24,19 @@
 #define TESTASSERT(COND) if(not (COND)){\
     throw std::runtime_error(#COND " | " __FILE__ " at line " TESTSYS_STRINGIFY(__LINE__)); \
   }
+
+/* The TESTTHROW macro tests whether the code STMT throws an exception whose what()
+ * string contains WHAT_SUBSTR, and reports a failure if it does not.
+ */
+#define TESTTHROW(STMT,WHAT_SUBSTR) {bool testsys_testthrow_exception_thrown = false; \
+  try{ STMT ;} \
+  catch(std::exception & ex){ \
+    if(std::string(ex.what()).find(WHAT_SUBSTR) != std::string::npos){	\
+      testsys_testthrow_exception_thrown = true; \
+    } \
+  } \
+  if(not testsys_testthrow_exception_thrown){ \
+    throw std::runtime_error(#STMT " did not throw " #WHAT_SUBSTR " | " \
+			     __FILE__ " at line " TESTSYS_STRINGIFY(__LINE__)); \
+  } \
+}

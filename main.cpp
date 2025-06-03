2e272791a7516e5b83fafedefe0f3b2a1a5e3992
Reuben Green <reubengreen183@gmail.com>
2025-06-03 12:10:24 +0100

    Removed some old code from main()

--- main.cpp
+++ main.cpp
@@ -2,21 +2,13 @@
 #include <fstream>
 
 #include "SegmentNumGenerator.h"
+#include "SecretKey.h"
 #include "PeerConfig.h"
+#include "ConfigFileParser.h"
 #include "Session.h"
 
 int main(){
   std::cout << "Hello!" << std::endl;
-
-  std::ofstream segnum_file("segnumfile",std::ios::trunc);
-  if(!segnum_file){
-       throw std::runtime_error(std::string("Could not open \"segnumfile\""));
-  }
-  segnum_file << std::to_string(0);
-  segnum_file.close();
-
-  Session session("segnumfile");
-  std::cout << session.segnumgen.next_num() << std::endl;
 
   return 0;
 }

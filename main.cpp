93b562f33f94866ee97ab82fc1f360ad95ae3343
Reuben Green <reubengreen183@gmail.com>
2025-09-11 15:28:00 +0100

    add a simple main() which just runs a Session with a config specified as a command line argument

--- main.cpp
+++ main.cpp
@@ -11,6 +11,10 @@
 #include "ConfigFileParser.h"
 #include "Session.h"
 
+/* A simple version of the main "cryptocomms" program, which just takes a config
+ * file and the name of a file to store the used segment numbers in, sets a Session
+ * running, and then just idles.
+ */
 int main(int argc, char** argv){
   if(argc != 3){
     std::cout << "Usage: " << argv[0] << " <config-file> <segnum-file>\n";
@@ -24,9 +28,8 @@ int main(int argc, char** argv){
                   cfp.self_ip_addr,cfp.self_port,
                   default_max_packet_size,
                   cfp.peer_configs,
-                  argv[2]);
-
-  session.start();
+                  argv[2],
+                  5);
 
   while(true)
     std::this_thread::sleep_for(std::chrono::milliseconds(1000));

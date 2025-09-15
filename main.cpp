678a8e51d2c58f749fecab70fb197f76cf9c1680
Reuben Green <reubengreen183@gmail.com>
2025-09-15 12:13:34 +0100

    updated main.cpp to get segment number file name from the config file

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <thread>
 #include <chrono>
+#include <string>
 
 #include "FifoIO.h"
 #include "UDPSocket.h"
@@ -12,23 +13,24 @@
 #include "Session.h"
 
 /* A simple version of the main "cryptocomms" program, which just takes a config
- * file and the name of a file to store the used segment numbers in, sets a Session
- * running, and then just idles.
+ * file, sets a Session running, and then just idles.
  */
 int main(int argc, char** argv){
-  if(argc != 3){
-    std::cout << "Usage: " << argv[0] << " <config-file> <segnum-file>\n";
+  if(argc != 2){
+    std::cout << "Usage: " << argv[0] << " <config-file>\n";
     return 0;
   }
 
   ConfigFileParser cfp(argv[1]);
   int default_max_packet_size = (cfp.default_max_packet_size == -1) ?
     1200 : cfp.default_max_packet_size;
+  std::string segnum_filepath = (cfp.segnum_filepath == "") ?
+    "segnumfile" : cfp.segnum_filepath;
   Session session(cfp.self_id,
                   cfp.self_ip_addr,cfp.self_port,
                   default_max_packet_size,
                   cfp.peer_configs,
-                  argv[2],
+                  segnum_filepath,
                   5);
 
   while(true)

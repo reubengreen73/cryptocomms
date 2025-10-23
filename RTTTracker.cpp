26ad0ca2896a41a1552f39494e55d193253561d7
Reuben Green <reubengreen183@gmail.com>
2025-10-23 10:22:01 +0100

    changed RTTTracker to use the std::uint_leastxx_t types

--- RTTTracker.cpp
+++ RTTTracker.cpp
@@ -1,9 +1,9 @@
 #include "RTTTracker.h"
 
-unsigned long int RTTTracker::current_rtt()
+std::uint_least32_t RTTTracker::current_rtt()
 { return current_rtt_; }
 
-void RTTTracker::update_rtt(unsigned long int rtt_measurement)
+void RTTTracker::update_rtt(std::uint_least32_t rtt_measurement)
 {
   if(unused_){
     current_rtt_ = rtt_measurement;
@@ -11,7 +11,7 @@ void RTTTracker::update_rtt(unsigned long int rtt_measurement)
   }
   else{
     /* the simple formula used here is from the original TCP specification */
-    long long int temp = current_rtt_-rtt_measurement;
+    std::uint_least64_t temp = current_rtt_-rtt_measurement;
     current_rtt_ = rtt_measurement + 0.9*temp;
   }
 }
--- RTTTracker.h
+++ RTTTracker.h
@@ -1,26 +1,27 @@
 /* stub version of RTTTracker which just uses simple averaging */
 
-/* NOTE: we use unsigned long int as the type for all round-trip times.
- * We use this rather than unsigned int since the C++ specification demands
- * that unsigned long int be at least 32 bits, while unsigned int must only
- * be 16 bits. Round-trip times are expressed in milliseconds, and (2^16 - 1)
- * milliseconds is about 65 seconds. Since it is possible we might in very
- * rare circumstances get a round-trip of more than 65 seconds, we use the
- * bigger type.
+/* NOTE: we use std::uint_least32_t as the type for all round-trip times.
+ * We use this rather than unsigned int since the C++ specification only
+ * demands that unsigned int must be at least 16 bits. Round-trip times
+ * are expressed in milliseconds, and (2^16 - 1) milliseconds is about 65
+ * seconds. Since it is possible we might in very rare circumstances get a
+ * round-trip of more than 65 seconds, we use the bigger type.
  */
 
 #ifndef RTTTRACKER_H
 #define RTTTRACKER_H
 
+#include <cstdint>
+
 class RTTTracker
 {
  public:
-  unsigned long int current_rtt();
-  void update_rtt(unsigned long int rtt_measurement);
+  std::uint_least32_t current_rtt();
+  void update_rtt(std::uint_least32_t rtt_measurement);
 
 private:
   bool unused_ = true;
-  unsigned long int current_rtt_;
+  std::uint_least32_t current_rtt_;
 };
 
 #endif
